e3b14ddafa44ca8e2fd036d7261a6da8a66b470b
joserandu <randujribeiro@gmail.com>
2024-11-18 14:06:12 -0300

    configuração do arquivo wsgi

--- src/projetoSecretaria/projetoSecretaria/wsgi.py
+++ src/projetoSecretaria/projetoSecretaria/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+# Add the project directory to the sys.path
+project_home = '/home/Randu/projeto-secretaria/src/projetoSecretaria'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'projetoSecretaria.settings'
+
+# Activate the virtualenv
+activate_this = '/home/Randu/projeto-secretaria/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Import Django WSGI handler to serve the application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetoSecretaria.settings')
-
 application = get_wsgi_application()
