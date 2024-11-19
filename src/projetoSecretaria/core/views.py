94f5c203ede2cca2fea89042ef34e34417242d9d
joserandu <randujribeiro@gmail.com>
2024-11-19 18:24:08 -0300

    Commit inicial para a Branch Develop

--- src/projetoSecretaria/core/views.py
+++ src/projetoSecretaria/core/views.py
@@ -8,3 +8,27 @@ def index(request):
 """
 Essa função é para reendenizar as páginas html.
 """
+
+
+def contar_alunos():
+    pass
+
+
+def contar_dias_aulas():
+    """já está feita"""
+    pass
+
+
+def contar_faltas_seguidas():
+    """A partir do contar_dias_aulas, vamos obter um índice, e para esse índice, contaremos dele até acharmos um x"""
+    pass
+
+
+def armazenar_faltantes():
+    """Será criado um dicionário com Nome do aluno, numero de faltas e número de telefone"""
+    pass
+
+
+def enviar_mensagem():
+    """Aqui será realizado o loop para o envio das mensagens"""
+    pass
