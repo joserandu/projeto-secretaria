65ed7d6a4cef6918664453b6767b3a5d96735504
Randú <134299499+joserandu@users.noreply.github.com>
2024-11-30 11:05:37 -0300

    Página de login concluída

--- src/projetoSecretaria/core/urls.py
+++ src/projetoSecretaria/core/urls.py
@@ -5,7 +5,7 @@ from .views import index, login, envio_mensagens_ccb, enviar_mensagens_ccb
 # As rotas da aplicação core ficarão aqui:
 urlpatterns = [
     path('', index, name="index"),
-    path('login', login),
+    path('login/', login, name="login"),
     path('envio_mensagens_ccb/', envio_mensagens_ccb, name='envio_mensagens_ccb'),
     path('enviar_mensagens_ccb/', enviar_mensagens_ccb, name='enviar_mensagens_ccb'),
 ]
--- src/projetoSecretaria/core/views.py
+++ src/projetoSecretaria/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .logic import main
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -9,13 +12,29 @@ def index(request):
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login_django(request, user)
+
+            return render(request, 'envio_mensagens_ccb.html')
+        else:
+            return HttpResponse("Email ou senha inválidos")
 
 
+@login_required(login_url="/login")
 def envio_mensagens_ccb(request):
     return render(request, 'envio_mensagens_ccb.html')
 
 
+# Sem o decorator:   if request.user.is_althenticated:
+@login_required(login_url="/login")
 def enviar_mensagens_ccb(request):
     main()
     return HttpResponse("Mensagens enviadas com sucesso!")
